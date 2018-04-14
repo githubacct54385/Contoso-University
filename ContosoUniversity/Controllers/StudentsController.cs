491c133689b32f101b5b50176e688a47c74eafe1
Alex <alexbarke002@gmail.com>
2018-04-14 16:51:48 +0800

    initial commit

---
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context) {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
